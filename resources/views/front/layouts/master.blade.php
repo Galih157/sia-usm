a25c445cce374ecedefbe3a1a2c5cfce7d59b641
Galih Nur Aditya <galihnuraditya@gmail.com>
2019-12-24 23:57:36 +0700

    Membuat halaman depan mahasiswa

---
+++ resources/views/front/layouts/master.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.min.css') }}">
+    @stack('custom_styles')
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-sm navbar-light bg-white shadow-sm">
+        <a class="navbar-brand" href="#">SIA USM</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa fa-home"></i> Home</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container-fluid py-2">
+        @yield('content')
+    </div>
+
+    <script src="{{ asset('assets/js/jquery-3.4.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    @stack('custom_scripts')
+</body>
+
+</html>
---
+++ resources/views/front/pages/home.blade.php
@@ -0,0 +1,42 @@
+@extends('front.layouts.master')
+
+@section('title') Hello World @endsection
+
+@section('content')
+<div class="row px-5">
+    <div class="col-sm-6 col-lg-4 offset-lg-8">
+        <div class="login-form shadow-sm">
+            <div class="text-center">
+                <div class="image-logo px-5 mb-1">
+                    <img src="{{ asset('images/logo-usm.png') }}" alt="">
+                </div>
+                <p>Selamat datang di SIA USM</p>
+            </div>
+            <div class="px-4">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="nim">NIM</label>
+                        <input type="text" name="nim" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Masuk</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('custom_styles')
+<link rel="stylesheet" href="{{ asset('vendor/front/home.css') }}">
+<style>
+    body {
+        background-image: url('{{ asset('images/leftside-background.jpg') }}');
+    }
+</style>
+@endpush
